2834d35bb918f44612bd2e05cbee743d9bf8b7c1
Rishabh <rs515666@gmail.com>
2024-11-27 10:26:31 +0530

    Created REST API for Guest

--- Receptionist/src/main/java/com/hotel/Receptionist/controller/GuestController.java
+++ Receptionist/src/main/java/com/hotel/Receptionist/controller/GuestController.java
@@ -1,9 +1,45 @@
 package com.hotel.Receptionist.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.hotel.Receptionist.model.Guest;
+import com.hotel.Receptionist.service.GuestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GuestController
 {
-    //
+    @Autowired
+    private GuestService service;
+
+    @GetMapping("/guestDetails/show")
+    public void showAllGuests()
+    {
+        //
+    }
+
+    @GetMapping("/guestDetails/show/{id}")
+    public void showGuestById(@PathVariable int id)
+    {
+        //
+    }
+
+    @PostMapping("/guestDetails/add")
+    public void addGuest(@RequestBody Guest guest)
+    {
+        //
+    }
+
+    @PutMapping("/guestDetails/update")
+    public void updateGuest(@RequestBody Guest guest)
+    {
+        //
+    }
+
+    @DeleteMapping("/guestDetails/delete/{id}")
+    public void deleteGuest(@PathVariable int id)
+    {
+        //
+    }
+
+
 }
