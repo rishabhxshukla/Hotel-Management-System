e3788b8280b87a819a522a1cd3c8e70399389c48
Thakur <amritansh.thakur@capgemini.com>
2024-11-27 10:42:16 +0530

    added Receptionist Service file

--- Receptionist/src/main/java/com/hotel/Receptionist/service/GuestService.java
+++ Receptionist/src/main/java/com/hotel/Receptionist/service/GuestService.java
@@ -1,9 +1,49 @@
 package com.hotel.Receptionist.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.hotel.Receptionist.model.Guest;
+import com.hotel.Receptionist.repository.GuestRepository;
 
 @Service
 public class GuestService
 {
-    //
+	
+	@Autowired
+    private GuestRepository repo;
+    
+	
+	// Will list all the guest name present in DB
+	public List<Guest> showAllGuest()
+	{
+		return repo.findAll();
+	}
+    
+	// Will give guest by there id
+	public Optional<Guest> showGuestById(int id)
+	{
+		return repo.findById(id);
+	}
+	
+	// Will add guest
+	public void addGuest(Guest guest)
+	{
+		repo.save(guest);
+	}
+	
+	//Will update guest
+	public void updateGuest(Guest guest)
+	{
+	   repo.save(guest);
+	}
+	
+	//Will delete guest by id
+	public void deleteGuest(int id)
+	{
+		repo.deleteById(id);
+	}
 }
