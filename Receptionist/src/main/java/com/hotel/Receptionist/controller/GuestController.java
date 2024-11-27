bb4bc95bd9ec84248c74954308fad373d602fbab
Rishabh <rs515666@gmail.com>
2024-11-27 11:20:10 +0530

    Integrated controller & service

--- Receptionist/src/main/java/com/hotel/Receptionist/controller/GuestController.java
+++ Receptionist/src/main/java/com/hotel/Receptionist/controller/GuestController.java
@@ -2,6 +2,9 @@ package com.hotel.Receptionist.controller;
 
 import com.hotel.Receptionist.model.Guest;
 import com.hotel.Receptionist.service.GuestService;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,34 +15,32 @@ public class GuestController
     private GuestService service;
 
     @GetMapping("/guestDetails/show")
-    public void showAllGuests()
+    public List<Guest> showAllGuests()
     {
-        //
+        return service.showAllGuest();
     }
 
     @GetMapping("/guestDetails/show/{id}")
-    public void showGuestById(@PathVariable int id)
+    public Optional<Guest> showGuestById(@PathVariable int id)
     {
-        //
+        return service.showGuestById(id);
     }
 
     @PostMapping("/guestDetails/add")
     public void addGuest(@RequestBody Guest guest)
     {
-        //
+        service.addGuest(guest);
     }
 
     @PutMapping("/guestDetails/update")
     public void updateGuest(@RequestBody Guest guest)
     {
-        //
+        service.updateGuest(guest);
     }
 
     @DeleteMapping("/guestDetails/delete/{id}")
     public void deleteGuest(@PathVariable int id)
     {
-        //
+        service.deleteGuest(id);
     }
-
-
 }
